d76a0ee598b11437667db9a90ef8a0d8f3618a2c
Brett Hughes <twofivinclimber@gmail.com>
2022-12-20 21:11:00 -0600

    fixing reactions

---
+++ components/Post/PostCard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+// import { Card, Image } from 'react-bootstrap';
+
+function PostCard() {
+  return (
+    // <Card>
+    //   <Card.Title>{postObj.title}</Card.Title>
+    //   <Image src={postObj.user_id.profile_image_url} />
+    //   <Card.Text>{postObj.user_id.first_name}{postObj.user_id.last_name}</Card.Text>
+    //   <Card.Body>
+    //   <Card.Text>{postObj}</Card.Text>
+    //   </Card.Body>
+    // </Card>
+    <>
+    </>
+  );
+}
+
+export default PostCard;
